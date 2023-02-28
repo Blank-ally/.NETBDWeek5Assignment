e9233e2001922b6c19addb9baf44d783a21f34dc
Blank-ally <lia.stith@gmail.com>
2023-02-28 16:01:44 -0600

    Add project files.

---
+++ Services/FileService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ApplicationTemplate.Services;
+
+/// <summary>
+///     This concrete service and method only exists an example.
+///     It can either be copied and modified, or deleted.
+/// </summary>
+public class FileService : IFileService
+{
+    private readonly ILogger<IFileService> _logger;
+   MovieList list = new MovieList();
+    Movie movie;
+
+    public FileService(ILogger<IFileService> logger)
+    {
+        _logger = logger;
+    }
+    public void Read()
+    {
+        _logger.Log(LogLevel.Information, "Reading");
+
+        list.getMovies();
+        Console.WriteLine("*** I am reading");
+    }
+
+    public void Write( Int64 id, string title, string[] genres)
+    {
+        _logger.Log(LogLevel.Information, "Writing");
+        movie = new Movie(id,title,genres);
+
+        string json = JsonConvert.SerializeObject(movie);
+        list.AddMovie(movie,json);
+        
+        Console.WriteLine("*** I am writing");
+    }
+}
---
+++ Services/MovieList.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTemplate.Services
+{
+    internal class MovieList
+    {
+       List<Movie> movies = new List<Movie>();
+
+        List<string> JsonMovies = new List<string>();
+
+        List<String> title = new List<string>();
+        List<Int64> Id = new List<Int64>();
+        public void AddMovie(Movie movie, String json)
+        {
+            JsonMovies.Add(json);
+          
+            title.Add(movie.Title);
+            Id.Add(movie.ID);
+            movie = JsonConvert.DeserializeObject<Movie>(json);
+            movies.Add(movie);
+        }
+
+        public void getMovies()
+        
+            {
+                foreach (Movie movie in movies)
+                {
+                Console.WriteLine(movie.Display());
+                
+                }
+            }
+
+      
+
+        public void getMovieTitles()
+        {
+            foreach (String title in title)
+            {
+                Console.WriteLine($"Title:{title}");
+            }
+        }
+        public void getMovieIDs()
+        {
+            foreach (Int64 id in Id) {
+                Console.WriteLine($"ID:{id}");
+
+            }
+        }
+
+
+    }
+}
